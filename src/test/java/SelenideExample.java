05075565033e8dc4825c4744cb0e20f89ad66697
Pavel Strunkin <pashidlos@gmail.com>
2020-05-16 16:24:49 +0200

    refactoring

--- src/test/java/SelenideExample.java
+++ src/test/java/SelenideExample.java
@@ -15,13 +15,13 @@ import java.io.IOException;
 
 import static com.codeborne.selenide.Selenide.$;
 
-public class Example {
+public class SelenideExample {
     VisualRegressionTracker visualRegressionTracker;
     Config config = new Config(
-            "http://localhost:4200",
-            "003f5fcf-6c5f-4f1f-a99f-82a697711382",
-            "F5Z2H0H2SNMXZVHX0EA4YQM1MGDD",
-            "develop"
+            "http://localhost:4200", // replace with your data
+            "31d57436-0924-4e19-86ca-9c5fd3bf32eb", // replace with your data
+            "BAZ0EG0PRH4CRQPH19ZKAVADBP9E", // replace with your data
+            "develop" // replace with your data
     );
 
     @BeforeSuite
@@ -45,7 +45,9 @@ public class Example {
         visualRegressionTracker.track(
                 "Home page",
                 ((TakesScreenshot) WebDriverRunner.getWebDriver()).getScreenshotAs(OutputType.BASE64),
-                TestRunOptions.builder().build()
+                TestRunOptions.builder()
+                        .diffTollerancePercent(0)
+                        .build()
         );
 
 
@@ -56,7 +58,12 @@ public class Example {
         visualRegressionTracker.track(
                 "Search result page",
                 ((TakesScreenshot) WebDriverRunner.getWebDriver()).getScreenshotAs(OutputType.BASE64),
-                TestRunOptions.builder().build()
+                TestRunOptions.builder()
+                        .browser("Chrome")
+                        .os("Windows")
+                        .viewport("1200x800")
+                        .diffTollerancePercent(1)
+                        .build()
         );
     }
 }
